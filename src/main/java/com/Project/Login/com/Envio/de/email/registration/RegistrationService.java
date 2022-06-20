cfde2c65203e615963c9281a160c17e4d2e656bc
Matheus Cavalcante <jmatheusoc269@gmail.com>
2022-06-20 02:15:22 -0300

    registration e suas configurações

---
+++ src/main/java/com/Project/Login/com/Envio/de/email/registration/RegistrationService.java
@@ -0,0 +1,7 @@
+package com.Project.Login.com.Envio.de.email.registration;
+
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return null;
+    }
+}
